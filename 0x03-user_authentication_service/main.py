c7ccfdb4e38b8e0d08826335499ea38fa36370ea
OMmienu <nkedzi@gmail.com>
2023-08-15 17:07:01 -0700

    main.py error fixed

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,13 +1,105 @@
 #!/usr/bin/env python3
 """
-Main file
+End to End Integration Test
 """
-from user import User
+import requests
 
-print(User.__tablename__)
 
-for column in User.__table__.columns:
-    print("{}: {}".format(column, column.type))
+def register_user(email: str, password: str) -> None:
+    """Tests user registration"""
+    response = requests.post(f"{BASE_URL}/users",
+                             data={
+                                 "email": email,
+                                 "password": password
+                             })
+    assert response.status_code == 200
+    assert response.json() == {"email": email, "message": "user created"}
+
+    response = requests.post(f"{BASE_URL}/users",
+                             data={
+                                 "email": email,
+                                 "password": password
+                             })
+
+    assert response.status_code == 400
+    assert response.json() == {"message": "email already registered"}
+
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """Tests whether the wrong password is rejected"""
+    response = requests.post(f"{BASE_URL}/sessions",
+                             data={
+                                 "email": email,
+                                 "password": password
+                             })
+    assert response.status_code == 401
+
+
+def profile_unlogged() -> None:
+    """Tests whether or not an unlogged user can access the profile"""
+    response = requests.get(f"{BASE_URL}/profile")
+    assert response.status_code == 403
+
+
+def log_in(email: str, password: str) -> str:
+    """Logs in a user"""
+    response = requests.post(f"{BASE_URL}/sessions",
+                             data={
+                                 "email": email,
+                                 "password": password
+                             })
+    assert response.status_code == 200
+    assert response.json() == {"email": EMAIL, "message": "logged in"}
+    session_id = response.cookies.get("session_id")
+    assert session_id is not None
+    return session_id
+
+
+def profile_logged(session_id: str) -> None:
+    """Tests whether or not a logged user can access the profile"""
+    response = requests.get(f"{BASE_URL}/profile",
+                            cookies={"session_id": session_id})
+    assert response.status_code == 200
+    assert response.json() == {"email": EMAIL}
+
+
+def log_out(session_id: str) -> None:
+    """Logs out a user"""
+    response = requests.delete(f"{BASE_URL}/sessions",
+                               cookies={"session_id": session_id})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Bienvenue"}
+    profile_unlogged()
+
+
+def reset_password_token(email: str) -> str:
+    """Password reset token"""
+    response = requests.post(f"{BASE_URL}/reset_password",
+                             data={"email": email})
+    assert response.status_code == 200
+    token = response.json().get("reset_token")
+    assert token is not None
+    return token
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """User can update their password with a reset token
+    """
+    response = requests.put(f"{BASE_URL}/reset_password",
+                            data={
+                                "email": email,
+                                "reset_token": reset_token,
+                                "new_password": new_password
+                            })
+    assert response.status_code == 200
+    assert response.json() == {"email": email, "message": "Password updated"}
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+BASE_URL = "http://localhost:5000"
+
 
 if __name__ == "__main__":
 
